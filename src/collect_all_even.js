278758573df11a6416d7923cad888d94b8535658
ZL1996 <649349296@qq.com>
2016-05-28 19:43:46 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,23 @@
-
 function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+  var result = [];
+  var JD = false;
+  for(var i=0;i<collection_a.length;i++){
+    for(var j=0;j<object_b.value.length;j++){
+       JD = is_Equal(collection_a[i].key,object_b.value[j])
+       if(JD)
+       result.push(collection_a[i].key);
+    }
+  }
+  return result;
+}
+
+function is_Equal(a,b){
+  if(a==b){
+    return true;
+  }
+  if(a!=b){
+    return false;
+  }
 }
 
 module.exports = collect_same_elements;
